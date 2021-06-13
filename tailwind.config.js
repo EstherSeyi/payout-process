274bb78fdff766a6bf652c87932d598e976d8ce9
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-13 16:42:07 +0100

    PP-001: Adds color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,34 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.ts", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      greyish: {
+        50: "#F2F2F2",
+        100: "#E5E5E5",
+        150: "#E8E8E8",
+        200: "#C4C5C6",
+        250: "#B7B7B7",
+        300: "#918DAB",
+        350: "#8B8F96",
+        400: "#636166",
+        450: "#404040",
+      },
+      purpleish: {
+        50: "#F4F3F8",
+        100: "#877E9E",
+        150: "#A98CF6",
+        200: "#4406EC",
+        250: "#4305EB",
+        300: "#2C0C6A",
+      },
+      blackish: {
+        50: "#222222",
+      },
+      misc: {
+        green: "#6FAE75",
+        white: "#FFFFFF",
+      },
+    },
     extend: {},
   },
   variants: {
