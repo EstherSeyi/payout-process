1e3eaa91d29b0a0a3f5add49acaaffb195482a27
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-16 16:36:56 +0100

    PP-003: makes nav style dynamic to show form progress

--- src/component/layout/index.tsx
+++ src/component/layout/index.tsx
@@ -4,20 +4,26 @@ import Logo from "../icons/Logo";
 import Close from "../icons/Close";
 import Navigation from "./Navigation";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ children, page }: { children: ReactNode; page: string }) => {
   return (
     <section className="bg-greyish-100 h-screen">
       <div className="bg-misc-white">
         <div className="flex w-11/12 sm:w-4/6 mx-auto justify-between p-4">
           <Logo />
-          <Navigation stylesClasses="max-w-lg w-4/6 hidden sm:block" />
+          <Navigation
+            page={page}
+            stylesClasses="max-w-lg w-4/6 hidden sm:block"
+          />
           <button>
             <Close color="#918DAB" />
           </button>
         </div>
       </div>
 
-      <Navigation stylesClasses="w-11/12 max-w-lg block sm:hidden mx-auto mt-8" />
+      <Navigation
+        page={page}
+        stylesClasses="w-11/12 max-w-lg block sm:hidden mx-auto mt-8"
+      />
 
       <div className="w-11/12 sm:w-4/6 mx-auto">{children}</div>
     </section>
