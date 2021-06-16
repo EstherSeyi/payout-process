cad1c22e9213508b7e33454215456a094ecefce7
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-16 15:11:52 +0100

    PP-003: temporal modification

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -1,9 +1,12 @@
-import Payout from "./Payout";
+// import Payout from "./Payout";
+// import Recipient from "./Recipient";
+
+import Review from "./Review";
 
 const Payment = () => {
   return (
     <div className="mt-16 sm:border border-greyish-550 max-w-lg mx-auto bg-misc-white p-6">
-      <Payout />
+      <Review />
     </div>
   );
 };
