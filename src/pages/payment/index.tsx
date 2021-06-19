0941fd168be90cc9ba779a935cac452c32f025e2
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-19 02:57:51 +0100

    PP-003: Modifies parent component

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -1,7 +1,11 @@
 import { lazy } from "react";
 import { useParams } from "react-router-dom";
+import { Formik } from "formik";
 
 import Layout from "../../component/layout";
+
+import { ValType } from "../../utils/types";
+import { validationSchema } from "../../validation/payout-form";
 
 const Review = lazy(() => import("./Review"));
 const Payout = lazy(() => import("./Payout"));
@@ -12,15 +16,37 @@ const Payment = () => {
 
   return (
     <Layout page={page}>
-      <div className="mt-16 sm:border border-greyish-550 max-w-lg mx-auto bg-misc-white p-6">
-        {page === "amount" ? (
-          <Payout />
-        ) : page === "recipient" ? (
-          <Recipient />
-        ) : page === "review" ? (
-          <Review />
-        ) : null}
-      </div>
+      <Formik
+        initialValues={{
+          from: "",
+          to: "",
+          send: "",
+          receive: "",
+          recipientEmail: "",
+          recipientName: "",
+          swiftOrBICcode: "",
+          accNumberOrIBAN: "",
+          transferFee: 0,
+          rate: 0,
+        }}
+        validationSchema={validationSchema}
+        onSubmit={async (values: ValType): Promise<void> => {
+          console.log(values);
+        }}
+      >
+        {(formik) => (
+          <form className="mt-16 sm:border border-greyish-550 max-w-lg mx-auto bg-misc-white p-6">
+            {console.log(formik.values)}
+            {page === "amount" ? (
+              <Payout formik={formik} />
+            ) : page === "recipient" ? (
+              <Recipient formik={formik} />
+            ) : page === "review" ? (
+              <Review formik={formik} />
+            ) : null}
+          </form>
+        )}
+      </Formik>
     </Layout>
   );
 };
