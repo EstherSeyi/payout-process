9a269a4929f928f40a902c0019b672292ac2058e
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-19 02:53:37 +0100

    PP-003: Updates Button component with new props

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,15 +1,30 @@
-import { ReactNode } from "react";
+import { ReactNode, MouseEventHandler } from "react";
 
 type ButtonProp = {
   children?: ReactNode;
   label?: string;
   styleClasses?: string;
+  type?: "submit" | "button" | "reset" | undefined;
+  disabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button = ({ children, label, styleClasses = "" }: ButtonProp) => {
+const Button = ({
+  children,
+  label,
+  styleClasses = "",
+  type = "button",
+  disabled = false,
+  onClick,
+}: ButtonProp) => {
   return (
     <button
-      className={`px-.5 py-2  w-full rounded text-xs sm:text-sm font-bold ${styleClasses}`}
+      type={type}
+      className={`px-.5 py-2  w-full rounded text-xs sm:text-sm font-bold focus:outline-none ${styleClasses} ${
+        disabled ? "cursor-not-allowed" : ""
+      }`}
+      disabled={disabled}
+      onClick={onClick}
     >
       {children || label}
     </button>
