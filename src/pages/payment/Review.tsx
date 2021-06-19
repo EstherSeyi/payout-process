aac1cc6cf2ef0b180c04ad6ee426c713d68c96af
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-19 02:58:34 +0100

    PP-003: Updates review component

--- src/pages/payment/Review.tsx
+++ src/pages/payment/Review.tsx
@@ -1,6 +1,11 @@
+import { FormikProps } from "formik";
+
 import Button from "../../component/Button";
 
-const Review = () => {
+import { ValType } from "../../utils/types";
+import { formatCurrency } from "../../helpers/format-data";
+
+const Review = ({ formik }: { formik: FormikProps<ValType> }) => {
   return (
     <>
       <p className="border-b border-greyish-550 pb-1 mb-6 text-base text-purpleish-300 font-semibold">
@@ -9,39 +14,50 @@ const Review = () => {
       <div className="border-b border-greyish-550 pb-1 text-13px mb-4">
         <div className="text-greyish-350 flex justify-between mb-2">
           <p>You send</p>
-          <p className="text-16px text-greyish-450 font-bold">1,000 USD</p>
+          <p className="text-16px text-greyish-450 font-bold">{`${formatCurrency(
+            Number(formik.values.send)
+          )} ${formik.values.from}`}</p>
         </div>
         <div className="text-greyish-350 flex justify-between mb-2">
           <p>Total fees (included)</p>
-          <p className=" text-greyish-450">3.69 USD</p>
+          <p className=" text-greyish-450">
+            {formatCurrency(formik.values.transferFee)}
+          </p>
         </div>
         <div className="text-greyish-350 flex justify-between mb-2">
           <p>Amount we’ll convert</p>
-          <p className=" text-greyish-450">996.31 USD</p>
+          <p className=" text-greyish-450">{`${formatCurrency(
+            Number(formik.values.send) - formik.values.transferFee
+          )}`}</p>
         </div>
         <div className="text-greyish-350 flex justify-between mb-2">
           <p>Guaranteed rate</p>
-          <p className=" text-greyish-450">1.10289</p>
+          <p className=" text-greyish-450">{formik.values.rate}</p>
         </div>
         <div className="text-greyish-350 flex justify-between mb-4">
-          <p>Johnny gets</p>
-          <p className="text-16px text-greyish-450 font-bold">1,248.63 EUR</p>
+          <p>{formik.values.recipientName.split(" ")[0] ?? "Recipient"} gets</p>
+          <p className="text-16px text-greyish-450 font-bold">{`${formatCurrency(
+            Number(formik.values.receive)
+          )} ${formik.values.to}`}</p>
         </div>
       </div>
       <div className="text-greyish-350 flex justify-between mb-2 text-13px">
         <p>Name</p>
-        <p className=" text-greyish-450">Johnny Gbadamosi</p>
+        <p className=" text-greyish-450">{formik.values.recipientName}</p>
       </div>
       <div className="text-greyish-350 flex justify-between mb-2 text-13px">
         <p>Email address</p>
-        <p className=" text-greyish-450">johnny.gbadamosi@gmail.com</p>
+        <p className=" text-greyish-450">{formik.values.recipientEmail}</p>
       </div>
       <div className="text-greyish-350 flex justify-between mb-2 text-13px">
         <p>IBAN / Account number</p>
-        <p className=" text-greyish-450">DE898919013902102</p>
+        <p className=" text-greyish-450">{formik.values.accNumberOrIBAN}</p>
       </div>
 
-      <Button styleClasses="bg-misc-green text-misc-white mt-8 py-3 focus:outline-none">
+      <Button
+        styleClasses="bg-misc-green text-misc-white mt-8 py-3 focus:outline-none"
+        type="submit"
+      >
         Confirm and continue
       </Button>
     </>
