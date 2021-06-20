7de49f85461b870cdb643e5d9e280cd0ab27bc32
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-20 10:21:54 +0100

    PP-004: Adds button test file

---
+++ src/__test__/Button.spec.ts
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import Button from "../component/Button";
+
+beforeEach(cleanup); //Clean up DOM
+
+describe('Button' =>(
+  test('expects Button ',()=>{
+
+
+
+
+  })
+))
