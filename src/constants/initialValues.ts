e09723ff4372dd97951b5f3c5723bf0c2f6f351f
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-20 10:23:25 +0100

    PP-004: Move initial values to separate page

---
+++ src/constants/initialValues.ts
@@ -0,0 +1,15 @@
+const initialValues = {
+  from: "",
+  to: "",
+  send: 0,
+  receive: 0,
+  recipientEmail: "",
+  recipientName: "",
+  swiftOrBICcode: "",
+  accNumberOrIBAN: "",
+  transferFee: 0,
+  rate: 0,
+  isEurope: false,
+};
+
+export default initialValues;
