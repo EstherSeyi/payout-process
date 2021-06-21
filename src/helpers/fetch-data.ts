063023aa8127f086e67fab75868e5d17f9ce64c9
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-06-21 04:20:01 +0100

    PP-004: changes http to https

--- src/helpers/fetch-data.ts
+++ src/helpers/fetch-data.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const fetch = async (from: string, to: string, amount: number) => {
   try {
     const { data } = await axios.get(
-      `http://data.fixer.io/api/convert?access_key=${process.env.REACT_APP_FIXER_ACCESS}&from=${from}&TO=${to}&amount=${amount}`
+      `https://data.fixer.io/api/convert?access_key=${process.env.REACT_APP_FIXER_ACCESS}&from=${from}&TO=${to}&amount=${amount}`
     );
     return data;
   } catch (error) {
